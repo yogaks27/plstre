f089dcf77be9370bdeee2a41e35bc0c6295e900c
YGAKS27. <143096199+yogaks27@users.noreply.github.com>
2024-07-28 20:57:01 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,56 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css?v=1.0.1',
+  '/script.js?v=1.0.1',
+  '/img/content.png',
+  '/testi/',
+  '/testi/index.html',
+  '/testi/img/content.png'
+];
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
