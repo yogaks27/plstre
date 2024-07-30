f0f42475739a74291ddc893d1f005ea20004d63c
YGAKS27. <143096199+yogaks27@users.noreply.github.com>
2024-07-30 19:26:10 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
+// Custom JS to hide loader
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        document.getElementById('loader').style.display = 'none';
+    }, 1000); // Adjust this delay as needed
+});
 
+// Disable text selection and context menu
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('copy', function (e) {
+        e.preventDefault();
+    });
+
+    document.body.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+
+    document.body.addEventListener('selectstart', function (e) {
+        e.preventDefault();
+    });
+});
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (error) {
+            console.log('ServiceWorker registration failed: ', error);
+        });
+    });
+}
